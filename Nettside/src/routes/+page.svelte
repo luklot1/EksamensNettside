20c324f7ccd0df0601cec7e3484cf17994a6644d
luklot1 <145323532+luklot1@users.noreply.github.com>
2024-05-30 13:49:21 +0200

    Update +page.svelte

--- Nettside/src/routes/+page.svelte
+++ Nettside/src/routes/+page.svelte
@@ -1,15 +1,65 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	let url1 = "https://github.com/luklot1/EksamensNettside"
+	let url2 = "https://github.com/luklot1/EksamensNettside"
+</script>
+ 
+<div class="absolute w-screen h-[510vh] bg-white">
+    <div class="flex w-full justify-center text-black">
+        <div class="flex flex-col w-full h-full">
+            <div class="flex text-3xl my-10 justify-center">
+                Knappen - Offisiell nettside
+            </div>
+            <div class="flex justify-center">
+                Knappen er et spill laget av Lukas Gulbrandsen Lotre. Dette er nettsiden du kan se <br>
+				offentlige oppdateringer, og nyheter om spillet. I tillegg er det her du kan se IM <br>
+				sine leaderboards.
+            </div>
+            
+            <div class="flex justify-center text-2xl my-3">
+                Hvordan fungerer spillet?
+            </div>
+            <div class="flex justify-center">
+                Spillet fungerer ved at det er en knapp, og en counter. Hver gang du trykker på <br>
+				knappen, vil counteren gå opp med 1. Men det er også en prosent sjanse for at <br>
+				counteren resetes. Denne sjansen starter på 0% for første gang, og vil så gå opp med 1% <br>
+				for hver gang du trykker, så det tallet som er på counteren er sjansen for at counteren vil <br> 
+				resete neste gang du trykker.
+            </div>
+        
+            
+            <div class="flex justify-center text-2xl my-3">
+                Hva er spillet laget med?
+            </div>
+            <div class="flex justify-center">
+                Jeg har brukt godot (gdscript), og firebase for å lage dette spillet. All funksjonalitet er laget <br>
+				i godot, mens firebase lagrer dine data slik at du forblir logget inn, beholder highscore, og holder <br>
+				styr på diverse statistikk.
+            </div>
+            
+			<div class="flex justify-center text-2xl my-3">
+                Github repo?
+            </div>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+            <div class="flex justify-center">
+				<div class="flex flex-col">
+					<div class="flex flex-col">
+						<a href="{url1}">Godot repo</a>
+					</div>
+					
+	
+	
+					<div class="flex flex-col">
+						<a href="{url2}">Nettside repo</a>
+					</div>
+				</div>
+				
+				
+				
+            </div>
+            
+        </div>    
+ 
+    </div>
+ 
 </div>
+ 
